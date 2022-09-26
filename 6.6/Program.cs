8877025a6f683af5768dd611a444088468130438
Adel <sitdikov-2015@inbox.ru>
2022-09-26 21:48:50 +0300

    new tasks added

---
+++ 6.6/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double code1 = 095;
+            double code2 = 812;
+            double code3 = 0163;
+            double code4 = 4242;
+            double price;
+            Console.WriteLine("Введите код:");
+            double code = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите время разговора:");
+            double time = double.Parse(Console.ReadLine());
+            if (code == code1)
+            {
+                price = time * 10;
+                Console.WriteLine("Код: 095, время разговора и стоимость:" + time + " мин, " + price + " руб.");
+            }
+            if (code == code2)
+            {
+                price = time * 10;
+                Console.WriteLine("Код: 812, время разговора и стоимость:" + time + " мин, " + price + " руб.");
+            }
+            if (code == code3)
+            {
+                price = time * 25.4;
+                Console.WriteLine("Код: 0163, время разговора и стоимость:" + time + " мин, " + price + " руб.");
+            }
+            if (code == code4)
+            {
+                price = time * 41.5;
+                Console.WriteLine("Код: 4242, время разговора и стоимость:" + time + " мин, " + price + " руб.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ SimpleMathTask1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleMathTask1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = double.Parse(Console.ReadLine());
+            double y = double.Parse(Console.ReadLine());
+            double f = Math.Exp(x);
+            double c = 0;
+            if ((x - y) == 0)
+                c = f * f + y * y + Math.Sin(y);
+            else if ((x - y) > 0)
+                c = (f - y) * (f - y) + Math.Cos(y);
+            else
+                c = (y - f) * (y - f) + Math.Tan(y);
+            Console.WriteLine("c = {0}", c);
+            Console.ReadKey();
+        }
+    }
+}
