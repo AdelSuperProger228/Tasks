fb0463dccb909ccc38b347be588bbd2a0fac9313
Adel <sitdikov-2015@inbox.ru>
2022-09-27 20:39:35 +0300

    icecream task fixed

--- IcecreamShop/Program.cs
+++ IcecreamShop/Program.cs
@@ -16,31 +16,36 @@ namespace IcecreamShop
             int money = 0;
             Console.WriteLine("Выберите сорт мороженого: \nшоколадное-100\nклубничное-120\nванильное-80:");
             string neededIceCream = Console.ReadLine();
-            Console.WriteLine("Выберите количество порций мороженого:");
-            int amount = Int32.Parse(Console.ReadLine());
-            
             switch (neededIceCream.ToLower())
             {
                 case "шоколадное":
-                    money = chocolate * amount;
-                    Console.WriteLine($"С вас: {money} рублей");
+                    money = chocolate;
                     break;
+
                 case "клубничное":
-                    money = strawberry * amount;
-                    Console.WriteLine($"С вас: {money} рублей");
+                    money = strawberry;
                     break;
+
                 case "ванильное":
-                    money = vanilla * amount;
-                    Console.WriteLine($"С вас: {money} рублей");
+                    money = vanilla;
                     break;
+
+                default:
+                    Console.WriteLine("Такого сорта нет");
+                    Console.ReadKey();
+                    return;
             }
+            Console.WriteLine("Выберите количество порций мороженого:");
+            int amount = Int32.Parse(Console.ReadLine());
+            int sum = money*amount;
+            Console.WriteLine($"С вас:{sum}, внесите оплату:");
             int pay = Int32.Parse(Console.ReadLine());
-            if (pay == money)
+            if (pay == sum)
                 Console.WriteLine("Платёж прошел, наслаждайтесь мороженым");
-            else if (pay > money)
-                Console.WriteLine($"Платёж прошел, ваша сдача: {pay - money} руб. наслаждайтесь мороженым");
+            else if (pay > sum)
+                Console.WriteLine($"Платёж прошел, ваша сдача: {pay - sum} руб. наслаждайтесь мороженым");
             else
-                Console.WriteLine("Денег недостаточно");
+                Console.WriteLine($"Денег недостаточно, вам нехватает {sum-pay} руб.");
 
             Console.ReadLine();
         }
